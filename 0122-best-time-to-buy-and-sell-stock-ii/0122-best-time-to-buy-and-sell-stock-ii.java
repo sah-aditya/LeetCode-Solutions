74ce1ac60136bf5239e6b5b98eb64eb3e030a32c
Aditya Sah <150173829+sah-aditya@users.noreply.github.com>
2026-03-20 19:08:50 +0530

    Time: 0 ms (100%), Space: 46.7 MB (12.04%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int start = prices[0];
+        int len= prices.length;
+
+        for(int i=1;i<len;i++){
+            if(prices[i]>start){
+                max+= prices[i]-start;
+            }
+            start= prices[i];
+        }
+        return max;
+        
+    }
+}
