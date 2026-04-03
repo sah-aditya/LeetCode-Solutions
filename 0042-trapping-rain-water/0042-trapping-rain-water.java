479d128a0ec387e80c1046d7abeefe166b1d8983
Aditya Sah <150173829+sah-aditya@users.noreply.github.com>
2026-04-03 23:42:22 +0530

    Time: 0 ms (100%), Space: 47.6 MB (75.66%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int trap(int[] arr) {
-        if (arr == null || arr.length == 0) return 0;
+    public int trap(int[] height) {
+        int left=0,right=height.length-1;
+        int leftmax=0, rightmax=0;
 
-        // Left max array
-        int lftmax = arr[0];
-        int[] lft = new int[arr.length];
-        lft[0] = lftmax;
-        for (int i = 1; i < arr.length; i++) {
-            lftmax = Math.max(lftmax, arr[i]);
-            lft[i] = lftmax;
+        int ans=0;
+        
+        while (left<right){
+            if(height[left]<height[right]){
+
+                if(height[left]> leftmax){
+                    leftmax= height[left];
+                }
+                else{
+                    ans+= leftmax- height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    ans+= rightmax- height[right];
+                }
+                right--;
+            }
         }
-
-        // Right max array
-        int rtmax = arr[arr.length - 1];
-        int[] rt = new int[arr.length];
-        rt[arr.length - 1] = rtmax;
-        for (int i = arr.length - 2; i >= 0; i--) {
-            rtmax = Math.max(rtmax, arr[i]);
-            rt[i] = rtmax;
-        }
-
-        // Calculate trapped water
-        int water = 0;
-        for (int i = 0; i < arr.length; i++) {
-            water += Math.min(lft[i], rt[i]) - arr[i];
-        }
-
-        return water;
+        return ans;
     }
 }
