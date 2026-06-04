15bedff2a7ccf2e42f5e0183fff12745b699ad0a
Aditya Sah <150173829+sah-aditya@users.noreply.github.com>
2026-06-04 22:41:08 +0530

    Time: 2 ms (90.91%), Space: 44.1 MB (70.82%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,58 +1,67 @@
-public class Solution {
-    int m, n;
-    
-    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
-    public int orangesRotting(int[][] matrix) {
-        m = matrix.length;
-        if (m == 0) return 0;
-        n = matrix[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        int freshOranges = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 2) {
-                    queue.offer(new int[] {i, j});
-                } else if (matrix[i][j] == 1) {
-                    freshOranges++;
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int row= grid.length;
+        int col= grid[0].length;
+
+        int fresh=0;
+        Queue <int[]> q = new LinkedList<>();
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
+                }
+
+                if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-        
-        if (freshOranges == 0) return 0;
-        
-        int minutes = 0;
-        
-        while (!queue.isEmpty() && freshOranges > 0) {
-            
-            minutes++;
-            
-            int size = queue.size();
-            
-            for (int k = 0; k < size; k++) {
-                int[] cell = queue.poll();
-                int r = cell[0];
-                int c = cell[1];
-                
-                for (int[] dir : directions) {
-                    int nr = r + dir[0];
-                    int nc = c + dir[1];
-                    
-                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] == 1) {
-                        
-                        matrix[nr][nc] = 2; 
-                        
-                        queue.offer(new int[] {nr, nc});
-                        
-                        freshOranges--;
-                    }
+
+        if(fresh==0){ return 0;}
+
+        int dir[][]={
+            {1,0},
+            {-1,0},
+            {0,1},
+            {0,-1}
+        };
+
+        int minutes=0;
+
+        while(!q.isEmpty()){
+            int size= q.size();
+
+            boolean rottenThisMinute= false;
+
+            for(int i=0;i<size;i++){
+                int curr[]= q.poll();
+                int r= curr[0];
+                int c= curr[1];
+
+                for(int d[]:dir){
+
+                    int nr= r+d[0];
+                    int nc= c+d[1];
+
+                    if( nr>=0 &&nr<row &&nc>=0 &&nc<col&&grid[nr][nc]==1){
+                            grid[nr][nc]=2;
+                            fresh--;
+                            q.offer(new int[]{nr,nc});
+
+                            rottenThisMinute=true;
+                        }
                 }
             }
+
+            if(rottenThisMinute){
+                minutes++;
+            }
         }
-        
-        return freshOranges == 0 ? minutes : -1;
+
+        if(fresh==0){
+            return minutes;
+        }
+        else return -1;
     }
 }
-
