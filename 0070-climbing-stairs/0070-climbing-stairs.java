42c2fc2cfba8e294f3bdc76e0366ea6247657366
Aditya Sah <150173829+sah-aditya@users.noreply.github.com>
2026-06-13 12:02:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (34.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,12 +3,16 @@ class Solution {
         if (n == 0 || n == 1) {
             return 1;
         }
-        int prev = 1, curr = 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+
         for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return curr;
+
+        return prev1;
     }
 }
